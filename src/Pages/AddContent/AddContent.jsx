e8606ccdc78efc8ff469c1fe45b381fab7b53758
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-12-23 04:37:51 +0600

    added add_content_page with photo prevew

--- src/Pages/AddContent/AddContent.jsx
+++ src/Pages/AddContent/AddContent.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import { RiCloseCircleFill } from "react-icons/ri";
+import { IoClose } from "react-icons/io5";
 
 const AddContent = () => {
   const [tags, setTags] = useState([]);
+  const [image, setImage] = useState(null);
+  const [formData, setFormData] = useState({});
 
+  console.log(formData);
+
+  // Save tags for press Enter key
   const handleKeyDown = (e) => {
-    console.log(e.target.value);
     if (e.key !== "Enter") return;
     const value = e.target.value;
     if (!value.trim()) return;
@@ -13,8 +18,17 @@ const AddContent = () => {
     e.target.value = "";
   };
 
+  // Removed selected Tag
   const handleRemoveTag = (index) => {
-    setTags(tags.filter((element, i) => i != index));
+    setTags(tags.filter((element, i) => i !== index));
+  };
+
+  // Set Image preview
+  const handleImagePreview = (e) => {
+    const image = e.target.files[0];
+    if (image) {
+      setImage(image);
+    }
   };
 
   return (
@@ -32,23 +46,57 @@ const AddContent = () => {
               </label>
 
               <div class="mb-1">
-                <input type="file" name="file" id="file" class="sr-only" />
-                <label
-                  for="file"
-                  class="relative flex min-h-[150px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] bg-white p-7 text-center"
-                >
-                  <div>
-                    <span class="mb-2 block text-xl font-semibold text-[#07074D]">
-                      Drop files here
-                    </span>
-                    <span class="mb-2 block text-base font-medium text-[#6B7280]">
-                      Or
-                    </span>
-                    <span class="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
-                      Browse
-                    </span>
+                <input
+                  type="file"
+                  accept="image/*"
+                  name="file"
+                  id="file"
+                  required
+                  onChange={handleImagePreview}
+                  class="sr-only"
+                />
+
+                {/* images Uploaded fields */}
+                {!image && (
+                  <label
+                    for="file"
+                    class="relative flex min-h-[250px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] bg-white p-7 text-center"
+                  >
+                    <div>
+                      <span class="mb-2 block text-xl font-semibold text-[#07074D]">
+                        Drop files here
+                      </span>
+                      <span class="mb-2 block text-base font-medium text-[#6B7280]">
+                        Or
+                      </span>
+                      <span class="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
+                        Browse
+                      </span>
+                    </div>
+                  </label>
+                )}
+
+                {/* images preview */}
+                {image && (
+                  <div
+                    style={{
+                      backgroundImage: `url(${URL.createObjectURL(image)})`,
+                      backgroundRepeat: "no-repeat",
+                      width: "100%",
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                    }}
+                    class="flex min-h-[250px] rounded-md border border-dashed border-[#e0e0e0] overflow-hidden relative"
+                  >
+                    <div
+                      title="Remove Image"
+                      onClick={() => setImage(null)}
+                      className="w-7 h-7 bg-gray-700 flex justify-center items-center rounded-full absolute top-2 right-2 border-2 border-white cursor-pointer"
+                    >
+                      <IoClose className="text-white text-2xl" />
+                    </div>
                   </div>
-                </label>
+                )}
               </div>
 
               <div class="mb-5 rounded-md bg-white py-4 px-8">
@@ -77,30 +125,29 @@ const AddContent = () => {
                   </label>
                   <textarea
                     id="content_details"
+                    required
                     rows="4"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Write your Content Details here..."
                   ></textarea>
                 </div>
 
+                {/* Add tag fields */}
                 <div className="flex gap-3 flex-wrap items-center bg-gray-50 rounded-lg border border-gray-300 p-2">
-                  {tags?.map((tag, index) => {
-                    console.log(tag, tags);
-                    return (
-                      <div
-                        key={index}
-                        className="px-2 py-1 rounded-lg bg-gray-300 font-medium inline-flex items-center mt-2"
+                  {tags?.map((tag, index) => (
+                    <div
+                      key={index}
+                      className="px-2 py-1 rounded-lg bg-gray-300 font-medium inline-flex items-center mt-2"
+                    >
+                      <span>{tag}</span>
+                      <span
+                        onClick={() => handleRemoveTag(index)}
+                        className=" text-black text-xl ml-1 cursor-pointer"
                       >
-                        <span>{tag}</span>
-                        <span
-                          onClick={() => handleRemoveTag(index)}
-                          className=" text-black text-xl ml-1 cursor-pointer"
-                        >
-                          <RiCloseCircleFill />
-                        </span>
-                      </div>
-                    );
-                  })}
+                        <RiCloseCircleFill />
+                      </span>
+                    </div>
+                  ))}
 
                   <input
                     className="px-2 py-1 mt-2 flex-grow border-none outline-none bg-gray-50 "
@@ -114,6 +161,7 @@ const AddContent = () => {
               </div>
             </div>
 
+            {/* Submit Button ( Note: there button tag not used because facing problem! ) */}
             <div>
               <span class="inline-block w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white cursor-pointer">
                 Send File
